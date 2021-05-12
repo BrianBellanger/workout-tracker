a3070978cd6b40638c0c87bbb20a2f710ae1b29e
Brian Bellanger <brianjbellanger@gmail.com>
2021-05-12 16:27:54 -0500

    Init

---
+++ Develop/models/Workout.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {type: Date, default: Date.now},
+    exercises: [
+        {
+            type: String,
+            name: String,
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number,
+        }
+    ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
