06408cf8cbdb84c8cfd2d7cf1001fecd74801d63
Brian Bellanger <brianjbellanger@gmail.com>
2021-05-12 21:05:24 -0500

    get workouts working

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const Workout = require('../models/WorkoutModel');
 
 router.get('/api/workouts', async (req, res) => {
+    console.log("workouts route hit!");
     Workout.aggregate([
         { $addFields:{totalDuration:{$sum:"$exercises.duration"}}}
     ])
@@ -14,49 +15,49 @@ router.get('/api/workouts', async (req, res) => {
     });
 
 
-Workout.find({})
-    .then((data) => {
-    console.log(data);
-    return res.status(200).json(data)
-    })
-    .catch((err) => {
-    return res.status(500).json(err)
-    });
+// Workout.find({})
+//     .then((data) => {
+//     console.log(data);
+//     return res.status(200).json(data)
+//     })
+//     .catch((err) => {
+//     return res.status(500).json(err)
+//     });
 });
 
 
-router.get('/api/workouts/range', async (req, res) => {
-    try {
-        const workoutData = await Workout.findAll();
-        return res.status(200).json(WorkoutData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.get('/api/workouts/range', async (req, res) => {
+//     try {
+//         const workoutData = await Workout.findAll();
+//         return res.status(200).json(WorkoutData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 
-router.post('/api/workouts', (req, res) => {
-    console.log(req.body);
-    Workout.create({})
-    .catch((err) => {
-        return res.status(500).json(err)
-    })
-});
+// router.post('/api/workouts', (req, res) => {
+//     console.log(req.body);
+//     Workout.create({})
+//     .catch((err) => {
+//         return res.status(500).json(err)
+//     })
+// });
 
 
-router.put("/api/workouts/:id", (req, res) => {
-    let excercise = req.body;
-    console.log(excercise);
-    Workout.findByIdAndUpdate(
-        req.params.id,
-        { $push: { exercises: exercise }})
-    .then((data) => {
-        return res.status(200).json(data)
-    })
-    .catch((err) => {
-        return res.status(500).json(err)
-    })
-});
+// router.put("/api/workouts/:id", (req, res) => {
+//     let excercise = req.body;
+//     console.log(excercise);
+//     Workout.findByIdAndUpdate(
+//         req.params.id,
+//         { $push: { exercises: exercise }})
+//     .then((data) => {
+//         return res.status(200).json(data)
+//     })
+//     .catch((err) => {
+//         return res.status(500).json(err)
+//     })
+// });
 
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -4,12 +4,11 @@ const apiRoutes = require('./routes/apiroutes');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-// app.use(apiRoutes);
-// app.use(htmlRoutes);
+app.use(apiRoutes);
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
