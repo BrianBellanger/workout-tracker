5220aadae8521b1e222bd28645beb135be4382c3
Brian Bellanger <brianjbellanger@gmail.com>
2021-05-13 19:00:26 -0500

    fix server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const htmlRoutes = require('./routes/htmlroutes');
-const apiRoutes = require('./routes/apiroutes');
+const htmlRoutes = require('./routes/htmlroutes.js');
+const apiRoutes = require('./routes/apiroutes.js');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
@@ -9,6 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(apiRoutes);
-require('./routes/htmlRoutes')(app);
+app.use(htmlRoutes);
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
